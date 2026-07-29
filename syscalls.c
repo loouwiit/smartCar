f6aeba2155a49db3191d03cd9f55c561ad7a011a
loouwiit <loouwiit@outlook.com>
2026-07-29 12:31:52 +0800

    feat(syscalls): 添加缺失的系统调用存根函数
    - 添加 `_close`、`_fstat`、`_isatty`、`_lseek`、`_read`、`_write` 六个系统调用存根
    - 包含必要的头文件 `sys/stat.h` 以便使用 `struct stat` 和 `S_IFCHR`
    - 修正 `errno` 的声明方式，使用 `#undef` 和 `extern int errno` 避免重复定义

--- syscalls.c
+++ syscalls.c
@@ -1,5 +1,9 @@
 #include <errno.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+
+#undef errno
+extern int errno;
 
 pid_t _getpid(void) {
     return 1;
@@ -8,4 +12,30 @@ pid_t _getpid(void) {
 int _kill(pid_t pid, int sig) {
     errno = ENOSYS;
     return -1;
+}
+
+int _close(int file) {
+    errno = EBADF;
+    return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+    st->st_mode = S_IFCHR;
+    return 0;
+}
+
+int _isatty(int file) {
+    return 1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+    return 0;
+}
+
+int _read(int file, char *ptr, int len) {
+    return 0;
+}
+
+int _write(int file, char *ptr, int len) {
+    return len;
 }
